1270a7e64bbbecbc701ed3eabd66f70bc59765e5
nutawat rodsomboon <68809531+salmonindaeyo@users.noreply.github.com>
2025-06-13 16:13:43 +0700

    test

--- iCash/Program.cs
+++ iCash/Program.cs
@@ -22,7 +22,7 @@ internal class Program
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            // hello The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
